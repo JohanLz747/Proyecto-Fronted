301ea1cb0face183e81664fe9e35ad39c42cf8be
johan <johan.lh2001@gmail.com>
2023-09-19 23:36:04 -0500

    inventario

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-//index
+/*index
 const user = JSON.parse(localStorage.getItem('login_success')) || false
 if(!user){
     window.location.href = 'login.html'
@@ -12,3 +12,38 @@ logout.addEventListener('click', ()=>{
     localStorage.removeItem('login_success')
     window.location.href = 'login.html'
 })
+*/
+
+class Product {
+    constructor(name, price , amoung) {
+        this.name = name;
+        this.price = price;
+        this.amoung = amoung;
+    }
+    addProduct() {
+
+    }
+
+}
+class UI {
+    addProduct(){
+
+    }
+    deletyeProduct(){
+        
+    }
+    message(){
+        
+    }
+}
+
+//DOOM
+document.getElementById('product-from')
+  .addEventListener('submit', function (e) {
+        const name = document.getElementById('name').value
+        const price = document.getElementById('price').value
+        const amoung = document.getElementById('amoung').value
+        console.log(name, price ,amoung);
+       const product = new Product(name, price, amoung);
+        e.preventDefault();
+  })
